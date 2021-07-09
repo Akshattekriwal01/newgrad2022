b59b893f6a8b4256b8d070b79e8ec0c86111b8ec
akshattekriwal01 <akshattekriwal123@gmail.com>
2021-07-09 06:17:34 -0400

    hide error

--- public/index.js
+++ public/index.js
@@ -282,6 +282,7 @@ if (el.classList.contains("red")) {
 function showError(error){
   document.getElementById("errorbox").innerHTML = error ;
   document.getElementById("errorbox").style.display = "block" ;
+  setTimeout(hideError(),1000);
 
 }
 function hideError(){
